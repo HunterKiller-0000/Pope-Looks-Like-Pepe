bbfc3bda1fb95e908b2887da222a5f11be290be7
HunterKiller-0000 <49916607+HunterKiller-0000@users.noreply.github.com>
2019-06-19 15:13:46 -0600

    lfd

---
+++ commands/ping.js
@@ -0,0 +1,16 @@
+const exampleEmbed = {
+	color: 0x0099ff,
+	title: 'Ping',
+	url: 'https://discord.js.org',
+	author: {
+		name: 'Some name',
+		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+		url: 'https://discord.js.org',
+	},
+	description: 'Some description here',
+	thumbnail: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	}
+};
+
+channel.send({ embed: exampleEmbed });
